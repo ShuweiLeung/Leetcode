26a57aba9b553a209a9f7db358a9e3b0ff8d881d
ShuweiLeung <36534502+ShuweiLeung@users.noreply.github.com>
2018-04-09 23:17:33 +0800

    Add files via upload

---
+++ String/17. Letter Combinations of a Phone Number/_17_LetterCombinationsOfAPhoneNumber.java
@@ -0,0 +1,50 @@
+/*
+Given a digit string, return all possible letter combinations that the number could represent.
+
+A mapping of digit to letters (just like on the telephone buttons) is given below.
+
+Input:Digit string "23"
+Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+
+Note:
+Although the above answer is in lexicographical order, your answer could be in any order you want. 
+ */
+
+package string.medium;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class _17_LetterCombinationsOfAPhoneNumber {
+	String[][] map = {{}, {}, {"a","b","c"}, {"d","e","f"}, {"g","h","i"}, {"j","k","l"}, {"m","n","o"}, {"p","q","r","s"}, {"t","u","v"}, {"w","x","y","z"}};
+	public static void main(String[] args) {
+		_17_LetterCombinationsOfAPhoneNumber test = new _17_LetterCombinationsOfAPhoneNumber();
+		List<String> list = test.letterCombinations("23");
+		for(String str:list) {
+			System.out.println(str);
+		}
+	}
+	
+	public List<String> letterCombinations(String digits) {
+		List<String> res = (List<String>)new ArrayList<String>();		//结果集
+		if(digits.length() == 0)
+			return res;
+		dfs(res, digits, 0, "");			//DFS深度搜索
+		return res;
+	}
+	
+	public void dfs(List<String> res, String digits, int depth, String path) {
+		if(depth == digits.length()) {
+			res.add(path);
+			return;
+		}
+		if(digits.charAt(depth) == '0' || digits.charAt(depth) == '1')	//digit为0或1时，没有代表的字母，所以跳过
+			dfs(res, digits, depth+1, path);
+		else {
+			int digit = Integer.valueOf(String.valueOf(digits.charAt(depth)));	//当前扫描到的digit
+			for(int i = 0; i < map[digit].length; i++) {
+				dfs(res, digits, depth+1, path+map[digit][i]);
+			}
+		}
+	}
+}
