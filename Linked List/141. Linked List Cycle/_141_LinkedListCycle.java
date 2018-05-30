ae667a3ffdb2503df04271e57e4d219ac851cedc
Shuwei Liang <36534502+ShuweiLeung@users.noreply.github.com>
2018-05-30 23:48:46 +0800

    Add files via upload

---
+++ Linked List/141. Linked List Cycle/_141_LinkedListCycle.java
@@ -0,0 +1,22 @@
+/*
+Given a linked list, determine if it has a cycle in it.
+
+Follow up:
+Can you solve it without using extra space? 
+ */
+
+package linkedlist.easy;
+
+public class _141_LinkedListCycle {
+	public boolean hasCycle(ListNode head) {
+	   if(head==null) return false;
+       ListNode walker = head;
+       ListNode runner = head;
+       while(runner.next!=null && runner.next.next!=null) {
+           walker = walker.next;
+           runner = runner.next.next;
+           if(walker==runner) return true;
+       }
+       return false;
+   }
+}
