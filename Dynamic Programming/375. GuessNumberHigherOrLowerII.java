eb512c13dcea32d349a4dfa3bded82cc37915cb6
Shuwei Liang <36534502+ShuweiLeung@users.noreply.github.com>
2018-07-19 18:31:59 +0800

    Update and rename _375_GuessNumberHigherOrLowerII.java to 375. GuessNumberHigherOrLowerII.java

--- Dynamic Programming/375. GuessNumberHigherOrLowerII.java
+++ Dynamic Programming/375. GuessNumberHigherOrLowerII.java
@@ -23,8 +23,6 @@ You end up paying $5 + $7 + $9 = $21.
 Given a particular n ≥ 1, find out how much money you need to have to guarantee a win.
  */
 
-package dynamicprogramming.medium;
-
 public class _375_GuessNumberHigherOrLowerII {
 	/**
 	 * top-down 带备忘录的递归DP解法
